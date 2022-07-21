c3cd314d6f7f1173aeb0169f8b503d832cc645fb
Vivek Rajput <indvivekrajput@gmail.com>
2022-07-21 18:24:13 +0530

    day-3 files

--- jiomart/src/App.jsx
+++ jiomart/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 // import './App.css'
 import Navbar from "./Components/Navbar"
-
+import AllRoutes from "./Routes/AllRouter"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     <Navbar/>
-
+    
+      <AllRoutes/>
     </div>
   )
 }
